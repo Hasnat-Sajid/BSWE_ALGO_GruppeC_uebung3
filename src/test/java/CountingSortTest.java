9181f8c597b365727318b1f9d057217d963fbc82
Markus Fehringer <2210859004@fh-burgenland.at>
2023-12-12 11:03:38 +0100

    split up unit test

--- src/test/java/CountingSortTest.java
+++ src/test/java/CountingSortTest.java
@@ -7,19 +7,24 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
  * Test class for the CountingSort implementation.
  * This class contains unit tests that verify the functionality
  * of the CountingSort algorithm under various conditions.
+ *
+ * Unit test naming convention: MethodName_StateUnderTest_ExpectedBehavior
  */
 public class CountingSortTest {
+    private final CountingSort countingSort = new CountingSort();
 
-    /**
-     * Tests the sort method of CountingSort.
-     * This test checks the correctness of the sorting algorithm with different types of input:
-     * a general case with a mix of numbers, an empty array, and an array with a single element.
-     */
     @Test
-    public void testSort() {
-        CountingSort countingSort = new CountingSort();
+    public void sort_UnsortedArray_SortedArray() {
         assertArrayEquals(new int[]{1, 2, 2, 3, 3, 4, 8}, countingSort.sort(new int[]{4, 2, 2, 8, 3, 3, 1}));
-        assertArrayEquals(new int[]{}, countingSort.sort(new int[]{})); // testen mit leerem Array
-        assertArrayEquals(new int[]{1}, countingSort.sort(new int[]{1})); // testen mit einem Element
+    }
+
+    @Test
+    public void sort_EmptyArray_EmptyArray() {
+        assertArrayEquals(new int[]{}, countingSort.sort(new int[]{}));
+    }
+
+    @Test
+    public void sort_SingleElement_SingleElement() {
+        assertArrayEquals(new int[]{1}, countingSort.sort(new int[]{1}));
     }
 }
