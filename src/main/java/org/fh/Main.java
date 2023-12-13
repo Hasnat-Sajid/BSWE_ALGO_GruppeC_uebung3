75e03346f4217ec0d118ec6be715c95c217ef3ea
Markus Fehringer <2210859004@fh-burgenland.at>
2023-12-13 21:29:57 +0100

    add bubble sort

--- src/main/java/org/fh/Main.java
+++ src/main/java/org/fh/Main.java
@@ -1,13 +1,14 @@
 package org.fh;
 
+import org.fh.Impl.BubbleSort;
 import org.fh.Impl.CountingSort;
 import org.fh.Interfaces.SortAlgorithm;
 import org.fh.Util.FileReaderUtil;
 
 /**
- * Main class for demonstrating the Counting Sort algorithm.
- * This class reads an array of integers from a file, sorts them using
- * the Counting Sort algorithm, and prints the sorted array.
+ * Main class for demonstrating the Counting Sort and Bubble Sort algorithm.
+ * This class reads an array of integers from a file, sorts then using
+ * the Counting Sort and Bubble Sort algorithm, and prints the sorted array.
  */
 public class Main {
     public static void main(String[] args) {
@@ -18,12 +19,25 @@ public class Main {
         int[] array = FileReaderUtil.readNumbersFromFile(filePath);
 
         // das sortieren mittels der klasse
-        SortAlgorithm algorithm = new CountingSort();
-        array = algorithm.sort(array);
+        SortAlgorithm countingSort = new CountingSort();
+        int[] sortedWithCounting = countingSort.sort(array);
 
         // ausgabe
-        System.out.println("Sortiertes Array: ");
-        for (int i : array) {
+        System.out.println("Sortiertes Array mit Counting Sort: ");
+        for (int i : sortedWithCounting) {
+            System.out.print(i + " ");
+        }
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+        SortAlgorithm bubbleSort = new BubbleSort();
+        int[] sortedWithBubble = bubbleSort.sort(array);
+
+        // ausgabe
+        System.out.println("Sortiertes Array mit Bubble Sort: ");
+        for (int i : sortedWithBubble) {
             System.out.print(i + " ");
         }
     }
